f28f662cc3c9ab9567caa91eab6f14a9f0426965
Omer717 <omer.shmaryhu40@gmail.com>
2022-01-19 15:47:48 +0200

    Socket PingPong works with a single client

--- PingPongClient/PingPongClient/Program.cs
+++ PingPongClient/PingPongClient/Program.cs
@@ -6,8 +6,7 @@ namespace PingPongClient
     {
         static void Main(string[] args)
         {
-            //var client = new Client(args[0], int.Parse(args[1]));
-            var client = new Client("127.0.0.1", 1337);
+            var client = new Client(args[0], int.Parse(args[1]));
             client.Start();
         }
     }
