ab65b5f1e0c48a7d5d34d9e8b364e93f5185102c
Omer717 <omer.shmaryhu40@gmail.com>
2022-01-19 16:14:38 +0200

    Client is working and finished

---
+++ PingPongClient/PingPongClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using PingPongClient.UI;
+using System;
+
+namespace PingPongClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var clientLogic = new Client("127.0.0.1", 1337);
+            var input = new ConsoleInput();
+            var output = new ConsoleOutput();
+
+            var client = new ClientWrapper(clientLogic, input, output);
+            client.RunPingPongClient();
+        }
+    }
+}
--- PingPongServer/PingPongServer/Program.cs
+++ PingPongServer/PingPongServer/Program.cs
@@ -6,7 +6,8 @@ namespace PingPongServer
     {
         static void Main(string[] args)
         {
-            var server = new Server(int.Parse(args[0]));
+            //var server = new Server(int.Parse(args[0]));
+            var server = new Server(1337);
             server.Start();
         }
     }
