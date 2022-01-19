80b6e860b4d31fd39f52136fc80ad5ded81823f7
Omer717 <omer.shmaryhu40@gmail.com>
2022-01-19 15:40:14 +0200

    Added Debug starts

--- PingPongClient/PingPongClient/Program.cs
+++ PingPongClient/PingPongClient/Program.cs
@@ -6,7 +6,8 @@ namespace PingPongClient
     {
         static void Main(string[] args)
         {
-            var client = new Client(args[0], int.Parse(args[1]));
+            //var client = new Client(args[0], int.Parse(args[1]));
+            var client = new Client("127.0.0.1", 1337);
             client.Start();
         }
     }
