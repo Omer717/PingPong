bb370d7fadee2afcc989efefbdbb3935ddb06aa2
Omer717 <omer.shmaryhu40@gmail.com>
2022-01-19 15:05:19 +0200

    Added IClient

---
+++ PingPongClient/PingPongClient/Abstractions/IClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongClient.Abstractions
+{
+    public interface IClient
+    {
+        void SendMessage(string message);
+        string RecieveMessage();
+    }
+}
